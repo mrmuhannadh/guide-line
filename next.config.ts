0596a46142a91acee61f1bf33fc54edb2c5313f7
mrmuhannadh <mrmuhannadh@gmail.com>
2025-11-10 18:08:27 +0530

    init commit

---
+++ next.config.ts
@@ -0,0 +1,31 @@
+import type { NextConfig } from 'next';
+import nextra from 'nextra';
+import { version } from './package.json';
+
+const basePath = process.env.BASE_PATH || '';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // mandatory, otherwise won't export
+  },
+  env: {
+    APP_VERSION: version,
+  },
+  basePath,
+  assetPrefix: `${basePath}/`,
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+  },
+};
+
+const withNextra = nextra({
+  // ... Other Nextra config options
+  unstable_shouldAddLocaleToLinks: true,
+});
+
+export default withNextra(nextConfig);
